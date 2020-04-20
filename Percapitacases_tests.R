f1b3169cd0073fd185418f73ed521f847a45910c
NK19 <nita.kanney@gmail.com>
2020-04-20 12:50:40 -0400

    Utilizing data on the number of confirmed cases and tests, code calaculates the rate of testing, infection, and number positive.

---
+++ Percapitacases_tests.R
@@ -0,0 +1,48 @@
+# Read in excel file data
+cases.tests <- read.csv("covid-19-total-confirmed-cases-vs-total-tests-conducted.csv", stringsAsFactors = FALSE)
+
+
+#Format the date column so that it is recognized as a date "YYYY-MM-DD"
+library(lubridate)
+cases.tests$Date <- mdy(cases.tests$Date)
+
+
+#Creation of a dataframe for subsetting of the data by date(March 30)
+dataframe1 <- subset(cases.tests, Date > as.Date("2020-03-30"))
+
+
+#Format USA label
+dataframe1$Entity <- as.character(dataframe1$Entity)
+dataframe1$Entity[dataframe1$Code == "USA"] <- "United States of Amerrica"
+
+
+
+# Read in excel file data
+popdata <- read.csv("WPP2019_TotalPopulationBySex.csv", stringsAsFactors = FALSE)
+
+
+#Create second data frame for population data
+dataframe2 <- subset(popdata, subset = Variant == "High" & Time == 2020)
+
+
+#Convert the total population to actual number by X 1,000
+dataframe2$PopTotal <- dataframe2$PopTotal * 1000
+
+
+#Merge data from both files into one dataframe
+dataframe_3 <- merge(dataframe1, dataframe2, by.x = c("Entity"), by.y = c("Location"))
+
+
+#Now, create a data frame with column data we wish to analyze
+df4 <- dataframe_3 [, c("Entity","Total.tests","Total.confirmed.cases.of.COVID.19..cases.","PopTotal")]
+
+
+#Combine the calculations with our dataframe we want to analyze
+df4$PerCapita.Testing <- df4$Total.tests / df4$PopTotal
+df4$PerCapita.Infection <- df4$Total.confirmed.cases.of.COVID.19..cases./ df4$PopTotal
+df4$COVID.Positive <- df4$Total.tests / df4$Total.confirmed.cases.of.COVID.19..cases.
+
+
+#Lastly, save your outputted data analysis as a csv
+write.csv(df4, file = "outputanalysis2.csv")
+
